a6753a9c46a034dc54bf3e52553693dcb84ddeff
Ashwin Gupta <ashwingupta2000@gmail.com>
2022-06-29 23:27:28 -0400

    set wm bar

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use x11::xlib;
+use std::os::raw::c_char;
+use std::ffi::CString;
+
+
 fn main() {
-    println!("Hello, world!");
+    let n = 0 as *const c_char;
+    let v = CString::new("Hello, world!").unwrap();
+    unsafe {
+        let dpy = xlib::XOpenDisplay(n);
+        let screen = xlib::XDefaultScreen(dpy);
+        let root = xlib::XRootWindow(dpy, screen);
+        xlib::XStoreName(dpy, root, v.as_ptr());
+        xlib::XFlush(dpy);
+    }
+    println!("Hello");
+
 }
