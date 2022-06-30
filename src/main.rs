8777da8582f91395da313034c330cde3bb0ff4fc
Ashwin Gupta <ashwingupta2000@gmail.com>
2022-06-30 02:06:11 -0400

    nullptr

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,25 @@
 use x11::xlib;
-use std::os::raw::c_char;
-use std::os::raw::c_int;
-use std::os::raw::c_ulong;
+use std::os::raw::{c_int, c_ulong};
 use std::ffi::CString;
+use std::{thread, time};
 
 //mod time_widget;
 
 fn main() {
-    let n = 0 as *const c_char;
     let v = CString::new("LOL").unwrap();
 
-    let dpy : *mut xlib::Display = unsafe {xlib::XOpenDisplay(n)};
+    let dpy : *mut xlib::Display = unsafe {xlib::XOpenDisplay(std::ptr::null())};
     let screen : c_int = unsafe{xlib::XDefaultScreen(dpy)};
     let win : c_ulong = unsafe{xlib::XRootWindow(dpy, screen)};
 
-    unsafe {
-        xlib::XStoreName(dpy, win, v.as_ptr());
-        xlib::XFlush(dpy);
-    }
     loop {
         unsafe {
             xlib::XStoreName(dpy, win, v.as_ptr());
             xlib::XFlush(dpy);
         }
+        thread::sleep(time::Duration::from_millis(1000))
     }
+
+    // Free dpy, screen, and win?
 
 }
