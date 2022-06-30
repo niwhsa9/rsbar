045399e365fdbf5269ec7fadfdad703b6183c43f
Ashwin Gupta <ashwingupta2000@gmail.com>
2022-06-30 01:58:09 -0400

    fix to scope

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
 use x11::xlib;
 use std::os::raw::c_char;
+use std::os::raw::c_int;
+use std::os::raw::c_ulong;
 use std::ffi::CString;
 
+//mod time_widget;
 
 fn main() {
     let n = 0 as *const c_char;
-    let v = CString::new("Hello, world! ").unwrap();
+    let v = CString::new("LOL").unwrap();
+
+    let dpy : *mut xlib::Display = unsafe {xlib::XOpenDisplay(n)};
+    let screen : c_int = unsafe{xlib::XDefaultScreen(dpy)};
+    let win : c_ulong = unsafe{xlib::XRootWindow(dpy, screen)};
+
     unsafe {
-        let dpy = xlib::XOpenDisplay(n);
-        let screen = xlib::XDefaultScreen(dpy);
-        let root = xlib::XRootWindow(dpy, screen);
-        xlib::XStoreName(dpy, root, v.as_ptr());
+        xlib::XStoreName(dpy, win, v.as_ptr());
         xlib::XFlush(dpy);
     }
-    println!("Hello");
+    loop {
+        unsafe {
+            xlib::XStoreName(dpy, win, v.as_ptr());
+            xlib::XFlush(dpy);
+        }
+    }
 
 }
