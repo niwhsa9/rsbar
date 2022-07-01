7ddd767bf001d331afc549a8608e4b8c0776c6f0
Ashwin Gupta <ashwingupta2000@gmail.com>
2022-07-01 19:11:09 -0400

    backup

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,21 @@ use std::os::raw::{c_int, c_ulong};
 use std::ffi::CString;
 use std::{thread, time};
 
-//mod time_widget;
+mod time_widget;
 
 fn main() {
-    let v = CString::new("LOL").unwrap();
+    let bar_text = CString::new("LOL").unwrap();
 
+    // X11 helpers
     let dpy : *mut xlib::Display = unsafe {xlib::XOpenDisplay(std::ptr::null())};
     let screen : c_int = unsafe{xlib::XDefaultScreen(dpy)};
     let win : c_ulong = unsafe{xlib::XRootWindow(dpy, screen)};
 
-    loop {
+    loop {  
+        // Determine bar text from modules
+
+
+        // Write to bar
         unsafe {
             xlib::XStoreName(dpy, win, v.as_ptr());
             xlib::XFlush(dpy);
@@ -20,6 +25,7 @@ fn main() {
         thread::sleep(time::Duration::from_millis(1000))
     }
 
-    // Free dpy, screen, and win?
+    // Free dpy
+    xlib::XCloseDisplay(dpy);
 
 }
