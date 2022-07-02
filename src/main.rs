907ee5a6e7a694930d10aea865dffca10be934aa
ashwin <ashwingupta2000@gmail.com>
2022-07-02 03:00:13 -0400

    time widget

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,10 @@ use std::os::raw::{c_int, c_ulong};
 use std::ffi::CString;
 use std::{thread, time};
 
-mod time_widget;
+use widgets::{TimeWidget, Widget};
+pub mod widgets;
 
 fn main() {
-    let bar_text = CString::new("LOL").unwrap();
-
     // X11 helpers
     let dpy : *mut xlib::Display = unsafe {xlib::XOpenDisplay(std::ptr::null())};
     let screen : c_int = unsafe{xlib::XDefaultScreen(dpy)};
@@ -16,16 +15,22 @@ fn main() {
     loop {  
         // Determine bar text from modules
 
+        let mut bar_text = String::new();
+        let time = TimeWidget {};
+        bar_text.push_str(&time.get_text());
+        let c_str : CString = CString::new(bar_text).unwrap();
 
         // Write to bar
         unsafe {
-            xlib::XStoreName(dpy, win, v.as_ptr());
+            xlib::XStoreName(dpy, win, c_str.as_ptr());
             xlib::XFlush(dpy);
         }
         thread::sleep(time::Duration::from_millis(1000))
     }
-
+    println!("Done");
     // Free dpy
-    xlib::XCloseDisplay(dpy);
+    unsafe {
+        xlib::XCloseDisplay(dpy);
+    }
 
 }
